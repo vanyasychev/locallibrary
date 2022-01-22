7418dac8e62601f5b5debd4dd56d1a7bfcf3242d
ivan <si.ivansychev@gmail.com>
2022-01-23 01:42:19 +0700

    Initial commit

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from . import views
+
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+]
